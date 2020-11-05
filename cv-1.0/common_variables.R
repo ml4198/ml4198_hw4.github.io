fd84fecf8c544ab9c9bd54ee936be0cf6434fb7c
Matt Lawlor <lawlormk@Macintosh.verizon.net>
2020-11-04 21:48:59 -0500

    index, cv, and site.yml

---
+++ cv-1.0/common_variables.R
@@ -0,0 +1,7 @@
+# Common non-csv data between CV and resume
+
+# Header with description of what is trying to be accomplished with this CV/Resume
+intro_text <- "I am a Fellow in Cardiology with clinical interest in structrual heart disease, cardiac CT, echocardiography, and interventional cardiology.
+
+Currently, I'm a research fellow and M.S. student in Patient Oriented Research expanding my skill set in data science, study design, and outcomes research.
+"
